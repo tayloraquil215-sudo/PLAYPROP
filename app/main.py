0e8c7027e0bad271017b8c600a73ff7638bc1247
tayloraquil215-sudo <tayloraquil215@gmail.com>
2026-01-18 18:21:06 -0500

    Update FastAPI title in main.py

--- app/main.py
+++ app/main.py
@@ -4,5 +4,5 @@ from routes import router
 
 Base.metadata.create_all(bind=engine)
 
-app = FastAPI(title="GitHub Sports Betting Tracker")
+app = FastAPI(title="Sports Betting Tracker")
 app.include_router(router)
